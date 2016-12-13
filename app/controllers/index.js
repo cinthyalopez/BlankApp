91f489ef4f57479e5e304d4462788f6d059f5013
Carolina Lopez <programminghamster@gmail.com>
2016-12-13 17:28:19 -0600

    Add sample code for scrolling content.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,39 @@
-function doClick(e) {
-    alert($.label.text);
+
+const LOG_TAG = '\x1b[31m' + '[index.js]' + '\x1b[39;49m ';
+
+function init() {
+	$.container.open();
 }
 
-$.index.open();
+function doClick(e) {
+    alert($.label.text);
+    // It won't print doClick()!
+    console.log(LOG_TAG, 'doClick()');
+
+}
+
+function placeholder(_evt) {
+	console.log('nothing');
+}
+
+function handleShowAlertButton(_evt) {
+	alert('This is an alert with a very loooooooooooooooooooooooooong message /%·%!/·)"·=($&!&$');
+}
+
+function handleShowAlertDialogButton(_evt) {
+	var dialog = Ti.UI.createAlertDialog({
+	    message: 'The file has been deleted',
+	    ok: 'Okay',
+	    title: 'File Deleted'
+	  });
+	  dialog.show();
+}
+
+
+$.showLoadingIndicatorButton.addEventListener('click', placeholder);
+$.showRefreshingLoadingIndicatorButton.addEventListener('click', placeholder);
+
+$.showAlertButton.addEventListener('click', handleShowAlertButton);
+$.showAlertDialogButton.addEventListener('click', handleShowAlertDialogButton);
+
+init();
