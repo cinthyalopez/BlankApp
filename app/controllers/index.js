ff57aba9b8dfc1b538a4c55712d3fe7ba004f6ef
Carolina Lopez <programminghamster@gmail.com>
2017-01-03 15:04:55 -0600

    Add simple webview test.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,17 @@ function doClick(e) {
     alert($.label.text);
 }
 
-$.index.open();
+function init() {
+	$.index.open();
+	if (OS_ANDROID) {
+		var intent = Ti.Android.createIntent({
+		    action : Ti.Android.ACTION_VIEW,
+		    data : 'http://net.educause.edu/ir/library/pdf/erm0342.pdf'
+		});
+		intent.addCategory(Ti.Android.CATEGORY_DEFAULT);
+		Ti.Android.currentActivity.startActivity(intent);
+	}
+	
+}
+
+init();
