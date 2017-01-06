f5bf3ff4b7588ab7ea3723b281208f1fd886f6a0
Carolina Lopez <programminghamster@gmail.com>
2017-01-06 16:39:52 -0600

    Add missing date parameters

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,10 @@
 var dateTimePicker = Ti.UI.createPicker({
 	  type:Ti.UI.PICKER_TYPE_DATE_AND_TIME,
 	  left: 0,
-	  height: 20,
-	  width: 300, 
 	  top:250,
+	  minDate:new Date(2009,0,1),
+	  maxDate:new Date(2014,11,31),
+	  value:new Date(2014,3,12),
 	  backgroundColor: 'green'
 	});
 	$.container.add(dateTimePicker);
