b0704d62fb41e3ef21eeb93f725329f5d6d29854
Carolina Lopez <programminghamster@gmail.com>
2017-01-13 11:44:18 -0600

    Fix variable name.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,6 @@ $.index.open();
 
 Ti.Network.addEventListener('change', function (_evt) {
 	alert('Network online?' + _evt.online);
-	$.networkStatusLabel.text = _evt.online;
+	$.label.text = _evt.online;
 	console.log('_evt=' + JSON.stringify(_evt));
 });
