8ecc26be629e350c4025c15e2c1a9205afeeb2ce
Carolina Lopez <programminghamster@gmail.com>
2017-01-17 10:35:57 -0600

    Add simple tab group test.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,26 @@
-function doClick(e) {
-    alert($.label.text);
-}
+$.tg.open();
 
-$.index.open();
+// var win1 = Ti.UI.createWindow({
+//     backgroundColor: 'blue',
+//     title: 'Blue'
+// });
+// win1.add(Ti.UI.createLabel({text: 'I am a blue window.'}));
+
+// var win2 = Ti.UI.createWindow({
+//     backgroundColor: 'red',
+//     title: 'Red'
+// });
+// win2.add(Ti.UI.createLabel({text: 'I am a red window.'}));
+
+// var tab1 = Ti.UI.createTab({
+//     window: win1,
+//     title: 'Blue'
+// }),
+// tab2 = Ti.UI.createTab({
+//     window: win2,
+//     title: 'Red'
+// }),
+// tabGroup = Ti.UI.createTabGroup({
+//     tabs: [tab1, tab2]
+// });
+// tabGroup.open();
