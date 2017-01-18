649778cc06ee226026a8f6ca5f3771654f95cfdc
Carolina Lopez <programminghamster@gmail.com>
2017-01-18 15:49:41 -0600

    Add simple button test.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,9 @@
-function doClick(e) {
-    alert($.label.text);
+function clickAlert(_evt) {
+    alert('click!');
 }
+
+$.touchEnabledTrueButton.addEventListener('click', clickAlert);
+$.touchEnabledFalseButton.addEventListener('click', clickAlert);
+
 
 $.index.open();
