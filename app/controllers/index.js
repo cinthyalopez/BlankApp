1d1d3efbf9c91419834b0b34e1d6adb060f607d8
Carolina Lopez <programminghamster@gmail.com>
2017-04-13 11:30:44 -0400

    Enhance touchmove test case a bit.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,9 +2,11 @@ function doClick(e) {
     alert($.label.text);
 }
 
-$.index.addEventListener('touchmove', function(_evt){
+$.view.addEventListener('touchmove', function(_evt){
 	console.log('_evt.x=' + _evt.x);
 	//console.log('_evt.y=' + _evt.y);
+	$.view.top += _evt.source.top;
+	$.view.left += _evt.source.left;
 });
 
 $.index.open();
