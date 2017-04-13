6eab2df7a514360af7387148d1b937d290ec955e
Carolina Lopez <programminghamster@gmail.com>
2017-04-13 09:18:24 -0500

    Add touchmove test event.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,9 @@ function doClick(e) {
     alert($.label.text);
 }
 
+$.index.addEventListener('touchmove', function(_evt){
+	console.log('_evt.x=' + _evt.x);
+	//console.log('_evt.y=' + _evt.y);
+});
+
 $.index.open();
