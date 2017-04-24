d70822c9e1b44c69374de22d459f579e8e8dd862
Carolina Lopez <programminghamster@gmail.com>
2017-04-24 14:00:00 -0500

    Simple video player sample.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,18 @@
-function doClick(e) {
-    alert($.label.text);
-}
+var vidWin = Titanium.UI.createWindow({
+    title : 'Video View Demo',
+    backgroundColor : '#fff'
+});
 
-$.index.open();
+var videoPlayer = Titanium.Media.createVideoPlayer({
+    top : 2,
+    autoplay : true,
+    backgroundColor : 'blue',
+    height : 300,
+    width : 300,
+    mediaControlStyle : Titanium.Media.VIDEO_CONTROL_VOLUME_ONLY,
+    scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FIT
+});
+
+videoPlayer.url = 'movie.mp4';
+vidWin.add(videoPlayer);
+vidWin.open();
