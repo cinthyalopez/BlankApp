a1db77731f34b8502f1f1211a861e4dca00db3ce
Carolina Lopez <programminghamster@gmail.com>
2017-05-05 17:07:01 -0500

    Print only one line for testing purposes.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,5 +5,5 @@ $.index.open();
 Ti.Network.addEventListener('change', function (_evt) {
 	alert('Network online?' + _evt.online);
 	$.label.text = _evt.online;
-	console.log('_evt=' + JSON.stringify(_evt));
+	console.log('network change event=' + _evt.online);
 });
