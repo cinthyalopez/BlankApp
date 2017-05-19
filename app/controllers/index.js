a363deacd25de9572174210a70f031135b196fe2
Carolina Lopez <programminghamster@gmail.com>
2017-05-19 10:25:10 -0500

    Upload basic sample of a list view with one section created with alloy markup.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,25 @@
-function doClick(e) {
-    alert($.label.text);
-}
+// Arguments passed into this controller can be accessed via the `$.args` object directly or:
+var args = $.args;
 
-$.index.open();
+var items = [
+	{
+		height: 40,
+		title: 'First',
+		accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE,
+		color: 'cyan'
+	},
+	{
+		height: 40,
+		title: 'Second',
+		accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE,
+		color: 'cyan'
+	},
+	{
+		height: 40,
+		title: 'Third',
+		accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE,
+		color: 'cyan'
+	}
+];
+
+$.section.setItems(items);
