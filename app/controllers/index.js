dada2c8f9a706bf6729fa2977b13232b2edf396e
Carolina Lopez <programminghamster@gmail.com>
2017-05-22 11:57:43 -0500

    Add sample code.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,60 +1,66 @@
+// $.window.open();
 
-
-$.label.addEventListener('click', function (_evt) {
-	var url = 'https://randomuser.me/api/?results=1500&nat=us';
-	var client = Ti.Network.createHTTPClient({
-		// function called when the response data is available
-		onload : function(e) {
-			console.log('e=' + JSON.stringify(e));
-
-			var response = JSON.parse(this.responseText);
-
-			console.log('response=' + response);
-
-			var userItems = _.map(response.results, function (_userObject) {
-				return {
-					properties: {
-						height: 40,
-						title: _userObject.name.first + ' ' + _userObject.name.last,
-						accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE,
-						color: 'black',
-						font: {
-							fontSize: 16
-						}
-					}
-				};
-			});
-
-			var searchBar = Ti.UI.createSearchBar({
-			    width: Ti.UI.FILL,
-			    height: '6%',
-			    backgroundColor: 'red'
-			});
-
-			var listView = Ti.UI.createListView({
-				searchView: searchBar,
-				width: Ti.UI.FILL, 
-				height: '94%'
-			});
-			var sections = [];
-
-			var userSection = Ti.UI.createListSection();
-			userSection.setItems(userItems);
-			sections.push(userSection);
-			listView.sections = sections;
-			$.window.add(searchBar);
-			$.window.add(listView);
-		},
-		// function called when an error occurs, including a timeout
-		onerror : function(e) {
-			alert('error');
-		},
-		timeout : 15000  // in milliseconds
-	});
-	// Prepare the connection.
-	client.open("GET", url);
-	// Send the request.
-	client.send();
+var win = Ti.UI.createWindow({
+	backgroundColor: 'white'
 });
 
-$.window.open();
+var searchBar = Ti.UI.createSearchBar({
+	backgroundColor: 'red'
+});
+
+var usersListView = Ti.UI.createListView({
+	height: Ti.UI.FILL,
+	width: Ti.UI.FILL,
+	backgroundColor: 'purple',
+	// searchView: searchBar
+});
+
+var sections = [];
+var userSection = Ti.UI.createListSection();
+sections.push(userSection);
+usersListView.sections = sections;
+
+var wrapper = Ti.UI.createView({
+	top: 0,
+	left: 0,
+	height: Ti.UI.FILL,
+	width: Ti.UI.FILL
+});
+
+wrapper.add(usersListView);
+win.add(wrapper);
+
+var url = 'https://randomuser.me/api/?results=150&nat=us';
+var client = Ti.Network.createHTTPClient({
+	// function called when the response data is available
+	onload : function(e) {
+		var response = JSON.parse(this.responseText);
+		var userItems = _.map(response.results, function (_userObject) {
+			return {
+				properties: {
+					height: 40,
+					title: _userObject.name.first + ' ' + _userObject.name.last,
+					accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE,
+					color: 'black',
+					font: {
+						fontSize: 16
+					}
+				}
+			};
+		});
+
+		userSection.setItems(userItems);
+		// $.section.setItems(userItems);
+	},
+	// function called when an error occurs, including a timeout
+	onerror : function(e) {
+		alert('error');
+	},
+	timeout : 15000  // in milliseconds
+});
+// Prepare the connection.
+client.open("GET", url);
+// Send the request.
+client.send();
+
+win.open();
