741a843b567c5e252c3969ffa9e0549f076c9eed
Carolina Lopez <programminghamster@gmail.com>
2017-06-21 16:46:24 -0500

    Add statements for trying to open the app in fullscreen.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,8 @@ function doClick(e) {
     alert($.label.text);
 }
 
+var ApplicationView = require('Windows.UI.ViewManagement.ApplicationView');
+var ApplicationViewWindowingMode = require('Windows.UI.ViewManagement.ApplicationViewWindowingMode');
+ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+
 $.index.open();
