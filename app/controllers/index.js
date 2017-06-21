4cbe601f1ae1ccb5d0d9f994ee8f8f33177a9e85
Carolina Lopez <programminghamster@gmail.com>
2017-06-21 16:16:41 -0500

    Modify open logic a little bit.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,8 @@
-function doClick(e) {
+function openTiWindow(_evt) {
     var window2 = Ti.UI.createWindow({
     	backgroundColor: 'blue'
     });
-    window2.open();
+    window2.open({ fullscreen: true });
 }
 
 $.index.open({ fullscreen: true });
