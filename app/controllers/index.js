dc0a278d9662f245360eb18ee61fed2fe342626e
Carolina Lopez <programminghamster@gmail.com>
2017-06-22 10:21:50 -0500

    Use SearchBar instead of textfield

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,14 @@
+var search = Titanium.UI.createSearchBar({
+    barColor:'#000', 
+    showCancel: true,
+    width: 350,
+    height: 143,
+    top: 0,
+    borderColor: 'red',
+    borderWidth: 5
+});
+
+$.container.add(search);
 
 
-$.index.open();
+$.container.open();
