2e7722988c84884c4a2623df61981ff2efb44c8e
Carolina Lopez <programminghamster@gmail.com>
2017-07-12 08:03:29 -0500

    Add simple tests of SearchBar events.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,12 +3,28 @@ var search = Titanium.UI.createSearchBar({
     showCancel: true,
     width: 350,
     height: 143,
-    top: 0,
-    borderColor: 'red',
-    borderWidth: 5
+    top: 0
+});
+
+
+search.addEventListener('blur', function (_evt) {
+	console.log('blur event fired!');
+});
+search.addEventListener('bookmark', function (_evt) {
+	console.log('bookmark event fired!');
+});
+search.addEventListener('cancel', function (_evt) {
+	console.log('cancel event fired!');
+});
+search.addEventListener('change', function (_evt) {
+	console.log('change event fired!');
+});
+search.addEventListener('focus', function (_evt) {
+	console.log('focus event fired!');
+});
+search.addEventListener('return', function (_evt) {
+	console.log('return event fired!');
 });
 
 $.container.add(search);
-
-
 $.container.open();
