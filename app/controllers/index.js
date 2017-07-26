83b73b3758e4ca74b61c74e40da40d9e8612bd1e
Cinthya Lopez <programminghamster@gmail.com>
2017-07-25 21:07:05 -0500

    Extend test case by attempting to change width and height as well

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,9 @@
-
-var value = true;
 console.log('$.square.backgroundColorChecked=' + $.square.backgroundColorChecked);
 console.log('$.square.backgroundColorUnchecked=' + $.square.backgroundColorUnchecked);
 	
-if (value) {
-	$.square.backgroundColor = $.square.backgroundColorChecked;
-} else {
-	$.square.backgroundColor = $.square.backgroundColorUnchecked;
-}
+$.square.backgroundColor = $.square.backgroundColorChecked;
+
+$.square.width = 50;
+$.square.height = 50;
 
 $.container.open();
