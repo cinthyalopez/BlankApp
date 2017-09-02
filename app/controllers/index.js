703c3397391da3ab66890ae2f6495f1779c6c0d0
Cinthya Lopez <programminghamster@gmail.com>
2017-09-01 22:34:46 -0500

    Updated test case to use Ti Classic.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,19 @@
-function doClick(e) {
-    alert($.label.text);
-}
+
+
+var win = Ti.UI.createWindow({
+	backround: 'magenta',
+	width: 500,
+	height: 500
+});
 
 var ApplicationView = require('Windows.UI.ViewManagement.ApplicationView');
 var ApplicationViewWindowingMode = require('Windows.UI.ViewManagement.ApplicationViewWindowingMode');
 ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
 
-$.index.open();
+var label = Ti.UI.createLabel({
+	height: Ti.UI.SIZE,
+	width: Ti.UI.SIZE,
+	text: 'Hellow World!'
+});
+win.add(label);
+win.open();
