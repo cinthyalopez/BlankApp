6ebc15aaf4211dc41ee59458d920d40082e14531
Cinthya Lopez <programminghamster@gmail.com>
2017-09-02 20:58:34 -0500

    Add simple test case.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,12 @@
+var win = Ti.UI.createWindow({ backgroundColor: 'white' });
+var button = Ti.UI.createButton({
+    title: 'BUTTON'
+});
+
+var style = Ti.UI.Windows.createStyle({ source: 'ms-appx:///Styles.xaml' });
+style.apply(button, 'ButtonStyle');
+win.add(button);
+win.open();
 
 
-$.container.open();
+// $.container.open();
