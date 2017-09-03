bfd7fe14cbf49ea2987afbed7d779bbd6eaca5d7
Cinthya Lopez <programminghamster@gmail.com>
2017-09-02 22:58:18 -0500

    Updated test case.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,8 +2,13 @@ console.log('$.square.backgroundColorChecked=' + $.square.backgroundColorChecked
 console.log('$.square.backgroundColorUnchecked=' + $.square.backgroundColorUnchecked);
 	
 $.square.backgroundColor = $.square.backgroundColorChecked;
+$.square.backgroundImage = $.square.backgroundImageChecked;
 
-$.square.width = 50;
-$.square.height = 50;
+$.square.width = 150;
+$.square.height = 150;
+
+$.text.color = $.text.colorChecked;
+
+$.button.touchEnabled = false;
 
 $.container.open();
