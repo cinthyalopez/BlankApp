f03cb8e7fe34385e6102688ab356e848f04a47e5
Cinthya Lopez <programminghamster@gmail.com>
2017-09-22 11:22:07 -0500

    Add background color in proxy to avoid color blinking.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 var win = Ti.UI.createWindow({ backgroundColor: 'white' });
 var button = Ti.UI.createButton({
-    title: 'BUTTON'
+    title: 'BUTTON',
+    backgroundColor: 'pink'
 });
 
 var style = Ti.UI.Windows.createStyle({ source: 'ms-appx:///Styles.xaml' });
