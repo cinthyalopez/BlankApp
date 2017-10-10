66f5eb8eac38bbfa00d65fb39cab4363e70f430c
Carolina Lopez <programminghamster@gmail.com>
2017-10-09 20:55:04 -0500

    Add initial test case.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,20 @@
-function doClick(e) {
-    alert($.label.text);
-}
+var view1 = Ti.UI.createView({
+    height: 200,
+    width: 200,
+    backgroundColor: 'orange'
+});
 
-$.index.open();
+var view2 = Ti.UI.createView({
+    height: 200,
+    width: 200,
+    backgroundColor: 'blue'
+});
+
+$.scrollView.add(view1);
+$.scrollView.add(view2);
+
+$.win.open();
+
+$.scrollView.removeAllChildren();
+$.scrollView.add(view1);
+$.scrollView.add(view2);
