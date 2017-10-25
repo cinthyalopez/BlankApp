8de9707f3d327058c1a340ac317f8f776a8fe631
Cinthya Lopez <programminghamster@gmail.com>
2017-10-25 11:39:40 -0500

    Add simple test case.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,10 @@
-function doClick(e) {
-    alert($.label.text);
-}
 
-$.index.open();
+$.outerWrapper.addEventListener('click', function (_evt) {
+	console.log('log from outerWrapper');
+});
+
+$.innerWrapper1.addEventListener('click', function (_evt) {
+	console.log('log from innerWrapper');
+});
+
+$.win.open();
