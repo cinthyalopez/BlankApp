7033329c888bbc6b1ef465922791d396e992684e
Cinthya Lopez <programminghamster@gmail.com>
2017-12-29 11:45:41 -0600

    Update code to work with SDK 7.0.0+

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,62 +1,62 @@
 var ListView = require('ti.xaml.listview');
 var win = Ti.UI.createWindow({ backgroundColor: 'green', layout: 'vertical' }),
 cmdView = Ti.UI.createView({
-	width: Ti.UI.FILL,
-	height: Ti.UI.SIZE,
-	layout: 'horizontal',
-	backgroundColor: 'gray'
+width: Ti.UI.FILL,
+height: Ti.UI.SIZE,
+layout: 'horizontal',
+backgroundColor: 'gray'
 }),
 goSection0 = Ti.UI.createButton({
-	title: 'Section 0'
+title: 'Section 0'
 }),
 goSection1 = Ti.UI.createButton({
-	title: 'Section 1'
+title: 'Section 1'
 }),
 goSection2 = Ti.UI.createButton({
-	title: 'Section 2'
+title: 'Section 2'
 });
 
 var items0 = [],
 items1 = [],
 items2 = [];
 
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 10; i++){
 	items0.push({ properties: { title: 'Fruit ' + i, image: 'Square150x150Logo.png' } });
 	items1.push({ properties: { title: 'Vegetable ' + i, image: 'Square150x150Logo.png' } });
 	items2.push({ properties: { title: 'Meat ' + i, image: 'Square150x150Logo.png' } });
 }
 
 var fruitSection = Ti.UI.createListSection({
-	headerTitle: 'Fruits',
-	items: items0
+headerTitle: 'Fruits',
+items: items0
 }),
 vegSection = Ti.UI.createListSection({
-	headerTitle: 'Vegetables',
-	items: items1
+headerTitle: 'Vegetables',
+items: items1
 }),
 meatSecion = Ti.UI.createListSection({
-	headerTitle: 'Meats',
-	items: items2
+headerTitle: 'Meats',
+items: items2
 });
 
 var listView = new ListView({
-	width: Ti.UI.FILL,
-	height: '80%',
-	sections: [fruitSection, vegSection, meatSecion]
+width: Ti.UI.FILL,
+height: '80%',
+sections: [fruitSection, vegSection, meatSecion]
 });
 
 goSection0.addEventListener('click', function () {
-	listView.scrollToItem(0, 0);
+listView.scrollToItem(0, 0);
 });
 goSection1.addEventListener('click', function () {
-	listView.scrollToItem(1, 0);
+listView.scrollToItem(1, 0);
 });
 goSection2.addEventListener('click', function () {
-	listView.scrollToItem(2, 0);
+listView.scrollToItem(2, 0);
 });
 
 listView.addEventListener('itemclick', function (e) {
-	alert(JSON.stringify(e.properties));
+alert(JSON.stringify(e.properties));
 });
 
 cmdView.add(goSection0);
