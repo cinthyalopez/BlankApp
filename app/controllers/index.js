9383b741ae1e150a363dcac639a266fe53786a85
Cinthya Lopez <programminghamster@gmail.com>
2018-08-08 16:00:17 -0500

    RSOD test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,9 @@
 function doClick(e) {
-    alert($.label.text);
+    var win = Ti.UI.createWindow({ backgroundColor: 'gray' });
+    win.addEventListener('open', function (e) {
+        win.add(null);
+    });
+    win.open();
 }
 
 $.index.open();
